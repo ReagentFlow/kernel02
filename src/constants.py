89be169958b8f1c986c40703451d2ff922eb7476
quzor <egor.khantakov@gmail.com>
2024-04-19 11:58:11 +0700

    serial port scanner fix

--- src/constants.py
+++ src/constants.py
@@ -1,7 +1,7 @@
 SCALES_PORT = 'COM3'
 KEY = 'private.json'
 COLLECTION = 'containers'
-SCANNER_PORT = '/dev/tty.Bluetooth-Incoming-Port'
+SCANNER_PORT = '/dev/tty.usbmodemS_N_G21AD97581'
 
 # video port for macbook pro 2017 /dev/cu.usbmodem00000000001A1
 # run port.py to find port for SCALES and SCANNER
